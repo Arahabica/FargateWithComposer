e610ff264de2e33c51ea32f93a1749ec5693bdc0
Arahabica <r.sasage@gmail.com>
2018-12-14 15:09:58 +0900

    nginx with express

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
 CLUSTER_NAME=test-express-2
-TARGET_GROUP=arn:aws:elasticloadbalancing:ap-northeast-1:696061999999:targetgroup/test-express-2/c907652a63f8fe5c
+TARGET_GROUP=arn:aws:elasticloadbalancing:ap-northeast-1:636069999999:targetgroup/test-express-2/c907652a63f8fe5c
 PROJECT_NAME=test-express-2
 
-AWS_ACCOUNT_ID=696061999999
+AWS_ACCOUNT_ID=63609999999
 AWS_REGION=ap-northeast-1
 
-#bash ./scripts/push.sh test-express-2-express . ${AWS_ACCOUNT_ID} ${AWS_REGION}
-bash ./scripts/push.sh test-express-2-nginx ./nginx ${AWS_ACCOUNT_ID} ${AWS_REGION}
+EXPRESS_REPO=test-express-2-express
+NGINX_REPO=test-express-2-nginx
+
+bash ./scripts/push.sh ${EXPRESS_REPO} . ${AWS_ACCOUNT_ID} ${AWS_REGION}
+bash ./scripts/push.sh ${NGINX_REPO} ./nginx ${AWS_ACCOUNT_ID} ${AWS_REGION}
 
 echo "AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID" > .env
 echo "AWS_REGION=$AWS_REGION" >> .env
+echo "EXPRESS_REPO=$EXPRESS_REPO" >> .env
+echo "NGINX_REPO=$NGINX_REPO" >> .env
 
 aws ecs create-cluster --cluster-name ${CLUSTER_NAME}
 
--- index.js
+++ index.js
@@ -4,4 +4,5 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 app.listen(3000, function () {
+  console.log("express started.")
 });
